44c4e65e8f262c1bcf91ecaa21b11335f71b4faf
Humairat-codes <humairat.codes@gmail.com>
2026-09-03 07:59:01 +0530

    hello world, variables, console.log()

---
+++ app.js
@@ -0,0 +1,18 @@
+// logs to the console
+console.log("Hello world!");
+// variables
+let a = 10;
+let f = 3.14;
+let name = "Dekisugi";
+let age = 22;
+let is_student = true;
+console.log(`Hi,${name},you are ${age} years old`);
+
+if(is_student){
+    console.log(`${name} is a student`);
+    
+}
+else{
+    console.log(`${name} is not a student`);
+    
+}
