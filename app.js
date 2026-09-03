0b4334796b3daaeae8eb51d78a527cb9955f6e6a
Humairat-codes <humairat.codes@gmail.com>
2026-09-03 08:36:54 +0530

    string manipulation

--- app.js
+++ app.js
@@ -28,3 +28,56 @@ console.log(x!==y);
 let z = '200';
 console.log(y==z); //true
 console.log(y!=z); //false
+
+// manipulating strinsg
+let fName = "Atherion";
+let lName = "Valerius";
+let fullName = fName + " " + lName;
+console.log(`${fName} ${lName}`);
+console.log(fullName);
+// string concat()
+let Name = fName.concat(" ",lName);
+console.log(Name);
+
+// append value
+fName += " VX";
+console.log(fName);
+
+// check length
+console.log(fName.length);
+console.log(lName.length);
+let Name1 = fName.concat(" ",lName);
+console.log(Name1.length);
+
+// cases
+console.log(Name1.toLowerCase());
+console.log(Name1.toUpperCase());
+
+// slice
+console.log(Name1.slice(0,2));
+console.log(Name1.slice(9,11));
+console.log(Name1.slice(15,));
+
+// split & join
+console.log(Name1.split(" ").join("-"));
+
+// includes
+console.log(Name1.includes("u"));
+console.log(Name1.includes("x"));
+
+// trim
+let movie = "           iron man                   ";
+console.log(movie);
+console.log(movie.trim()); //iron man
+
+let favCharFirstName = "L";
+let favCharLastName = "Lawliet";
+let favChar = favCharFirstName.concat(" ",favCharLastName);
+let upperCase = favChar.toUpperCase();
+let msg = `My favorite Anime character is ${upperCase} because he is intelligent`
+msg += `. He is from anime "Death Note`;
+console.log(msg);
+
+
+
+
