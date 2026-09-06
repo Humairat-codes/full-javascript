c825d078450c28a1d1ea0b6936d041427119aac9
Humairat-codes <humairat.codes@gmail.com>
2026-09-06 07:39:20 +0530

    loops

---
+++ ControlFlow/app1.js
@@ -0,0 +1,12 @@
+// for loops
+// DRY - DON'T REPEAT YOURSELF
+// for(let i = 1;i<=10;i++)
+// {
+//     console.log(i,"Omnix Codes");
+// }
+
+// let i = 10;
+// while(i<=100){
+//     console.log("Omnix codes",i);
+//     i++;
+// }
