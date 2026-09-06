541e3a692f9995ef502ac1a7609c1d5a66d85f97
Humairat-codes <humairat.codes@gmail.com>
2026-09-06 09:30:10 +0530

    loops

--- ControlFlow/app1.js
+++ ControlFlow/app1.js
@@ -16,3 +16,21 @@
 //     console.log(i);
 //     i++;
 // } while(i<=5);
+
+//LOGICAL OPERATORS
+// AND (&&)
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// OR(||)
+// console.log(true || true );
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// NOT(!)
+// console.log(!true);
+// console.log(!false);
+
