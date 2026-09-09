50d11f117ce21440a9429481ae934afe9acbb6cf
Humairat-codes <humairat.codes@gmail.com>
2026-09-09 20:12:13 +0530

    day 2

--- ControlFlow/app1.js
+++ ControlFlow/app1.js
@@ -1,36 +1,66 @@
-// for loops
-// DRY - DON'T REPEAT YOURSELF
-// for(let i = 1;i<=10;i++)
-// {
-//     console.log(i,"Omnix Codes");
+/*for loops
+DRY - DON'T REPEAT YOURSELF
+for(let i = 1;i<=10;i++)
+{
+    console.log(i,"Omnix Codes");
+}
+
+let i = 10;
+while(i<=100){
+    console.log("Omnix codes",i);
+    i++;
+}
+let i = 1;
+do
+{
+    console.log(i);
+    i++;
+} while(i<=5);
+*/
+/*
+LOGICAL OPERATORS
+AND (&&)
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+OR(||)
+console.log(true || true );
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+NOT(!)
+console.log(!true);
+console.log(!false);
+*/
+
+
+// ARRAYS
+const favSingers = ["Me","myself","I"]
+// console.log(favSingers[0]);
+const favNumbers = [1,2,3,4,5,6,8,9];
+const mixedArr = ["hey",["bonjour","salam","Hola"],"Anneyoeng haseayo",123,true]
+// for(let i =0;i<mixedArr.length;i++){
+//     console.log(mixedArr[i]);
+    
 // }
+// ARRAY TECHNIQUES 
+let a1 = [8,9,5,6];
+a2=a1.concat([4,88,99,65]);
+console.log(a2.includes(88));
+console.log(a1.push([10,55],[12,88],990,88));
+console.log(a1);
+console.log(a1.unshift(13));
 
-// let i = 10;
-// while(i<=100){
-//     console.log("Omnix codes",i);
-//     i++;
-// }
-// let i = 1;
-// do
-// {
-//     console.log(i);
-//     i++;
-// } while(i<=5);
+console.log(a1.pop());
+console.log(a1.shift());
+console.log(a1.sort());
+console.log(a1.slice(3,5));
+console.log(a1.join("*"));
 
-//LOGICAL OPERATORS
-// AND (&&)
-// console.log(true && true);
-// console.log(true && false);
-// console.log(false && true);
-// console.log(false && false);
+console.log(a1.splice());
 
-// OR(||)
-// console.log(true || true );
-// console.log(true || false);
-// console.log(false || true);
-// console.log(false || false);
 
-// NOT(!)
-// console.log(!true);
-// console.log(!false);
-
+console.log(a1);
